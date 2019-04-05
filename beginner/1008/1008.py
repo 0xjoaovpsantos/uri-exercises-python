54faa1f6bb86ca4ef2c67c6398446ec64ddc5f6b
João Victor Pereir Santos <joaovictorsantoscpv@gmail.com>
2019-04-05 07:49:45 -0300

    Add exercises beginners

---
+++ beginner/1008/1008.py
@@ -0,0 +1,7 @@
+numeroDoFuncionario = int(input())
+horasTrabalhadas = int(input())
+precoPorHora = float(input())
+
+salario = horasTrabalhadas * precoPorHora
+print("NUMBER = {}".format(numeroDoFuncionario))
+print("SALARY = U$ {:.2f}".format(salario))
